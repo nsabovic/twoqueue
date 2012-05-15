8aba3f11638ec617fab406a99e505e3c6a105638
Nebojsa Sabovic <nsabovic@gmail.com>
2012-05-14 17:36:25 -0700

    Added first()/last().

--- test/queue.js
+++ test/queue.js
@@ -65,4 +65,32 @@ describe('Queue', function() {
       assert.deepEqual([1, 2, 3], queue.dequeueMultiple(4));
     });
   });
+
+  describe('#first', function() {
+    it('returns the first element', function() {
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      assert.equal(queue.first(), 1);
+      queue.dequeue();
+      assert.equal(queue.first(), 2);
+      queue.enqueue(4);
+      queue.enqueue(5);
+      assert.equal(queue.first(), 2);
+    });
+  });
+
+  describe('#last', function() {
+    it('returns the last element', function() {
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      assert.equal(queue.last(), 3);
+      queue.dequeue();
+      assert.equal(queue.last(), 3);
+      queue.enqueue(4);
+      queue.enqueue(5);
+      assert.equal(queue.last(), 5);
+    });
+  });
 });
